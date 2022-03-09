3e40fb100dd6e1592c3d71df7a3a1204bda4e032
Christian Monrad Mortensen <chri615w@edu.sde.dk>
2022-03-09 14:30:08 +0100

    first datagrid

--- bibliotekDB_GUI/bibliotekDB_GUI/MainWindow.xaml.cs
+++ bibliotekDB_GUI/bibliotekDB_GUI/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace bibliotekDB_GUI
 {
@@ -23,6 +26,34 @@ namespace bibliotekDB_GUI
         public MainWindow()
         {
             InitializeComponent();
+            FillDataGrid();
+        }
+        private void FillDataGrid()
+
+        {
+
+            string ConString = ConfigurationManager.ConnectionStrings["MySqlServer"].ConnectionString;
+
+            string CmdString = string.Empty;
+
+            using (SqlConnection con = new SqlConnection(ConString))
+
+            {
+
+                CmdString = "SELECT * FROM Items";
+
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable("Items");
+
+                sda.Fill(dt);
+
+                ItemDataGrid.ItemsSource = dt.DefaultView;
+
+            }
+
         }
     }
     // Data Source=DESKTOP-5GIERKE;Initial Catalog=bibliotekdb;Integrated Security=True
